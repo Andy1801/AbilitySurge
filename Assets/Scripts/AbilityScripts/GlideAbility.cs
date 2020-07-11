ca5714f2446da9441d13e809a0b9ed0aa28a730b
Andy Garcia <andy89190@gmail.com>
2020-07-11 01:47:54 -0400

    Init glide ability files

---
+++ Assets/Scripts/AbilityScripts/GlideAbility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlideAbility : IAbilites
+{
+    const float gravityBuffer = 0.5f;
+
+    public bool actionCondition(GameObject player)
+    {
+        PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+        
+        return playerMovement.getIsGrounded();
+    }
+
+    public void action(GameObject player)
+    {
+        Rigidbody2D rigidbody2D = player.GetComponent<Rigidbody2D>();
+
+        rigidbody2D.gravityScale *= gravityBuffer;
+    }
+ 
+}

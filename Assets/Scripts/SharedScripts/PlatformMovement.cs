1d7ac9554595b01669b843d6bbeb01930281d6e1
Emtanis Shoukry <eshou003@fiu.edu>
2021-04-08 21:14:48 -0400

    Created Menu System

--- Assets/Scripts/SharedScripts/PlatformMovement.cs
+++ Assets/Scripts/SharedScripts/PlatformMovement.cs
@@ -4,42 +4,36 @@ using UnityEngine;
 
 //TODO Platform studdering when the player gets on it
 //TODO Move logic from grounded to here that involves moving the player when atop the platfom
-public class PlatformMovement : MonoBehaviour
-{
+public class PlatformMovement : MonoBehaviour {
     public float movementTimer;
     public Vector3 movementOffset;
 
     private Timer timer;
 
-    void Start()
-    {
-        timer = new Timer(movementTimer);
-        timer.StartTimer();
+    void Start () {
+        timer = new Timer (movementTimer);
+        timer.StartTimer ();
     }
 
-    void Update()
-    {
-        if (timer.getTimerStatus())
-            movementOffset = reversalMovementAndTimer();
+    void Update () {
+        if (timer.getTimerStatus ())
+            movementOffset = reversalMovementAndTimer ();
         else
             transform.position += (movementOffset * Time.deltaTime);
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
+    void OnCollisionEnter2D (Collision2D other) {
         if (other.gameObject.tag == "Ground")
-            movementOffset = reversalMovementAndTimer();
+            movementOffset = reversalMovementAndTimer ();
     }
 
-    void OnCollisionStay2D(Collision2D other)
-    {
-        Transform transform = other.gameObject.GetComponent<Transform>();
-        transform.Translate(movementOffset * Time.deltaTime, Space.World);
+    void OnCollisionStay2D (Collision2D other) {
+        Transform transform = other.gameObject.GetComponent<Transform> ();
+        transform.Translate (movementOffset * Time.deltaTime, Space.World);
     }
 
-    Vector3 reversalMovementAndTimer()
-    {
-        timer.StartTimer();
+    Vector3 reversalMovementAndTimer () {
+        timer.StartTimer ();
         return movementOffset *= -1;
     }
 }

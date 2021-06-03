5b1eed3e9c54262ecf913f278887d46db1bc5f12
Andy Garcia <andy89190@gmail.com>
2021-06-02 22:47:58 -0400

    Added health to the play scene

---
+++ Assets/Scripts/UIScripts/HealthUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    private Stack<Image> heartContainers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heartContainers = new Stack<Image>();
+
+        foreach (Transform childTransform in gameObject.transform.GetComponentInChildren<Transform>())
+        {
+            heartContainers.Push(childTransform.GetChild(0).GetComponent<Image>());
+        }
+        Debug.Log(heartContainers.Count);
+    }
+
+    // Uncomment in order to test health works
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.P))
+    //     {
+    //         reduceHeartContainers();
+    //     }
+    // }
+
+    public void reduceHeartContainers()
+    {
+        Image heartContainer = heartContainers.Count != 0 ? heartContainers.Pop() : null;
+
+        if (heartContainer != null)
+            heartContainer.fillAmount = 0;
+    }
+}
